ff8e5480cca7c3fd3fec5ac0b219aebbe341aa91
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2024-01-31 05:34:22 +0900

    메인페이지(4)
    -탭 클릭 기능 완성

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-//svg
+//Img
 import { ReactComponent as Title1Icon } from "../assets/main/title1.svg";
 import { ReactComponent as Title2Icon } from "../assets/main/title2.svg";
 import { ReactComponent as TapRound1 } from "../assets/main/tapRound1.svg";
@@ -18,6 +18,15 @@ import EatTitle2 from "../assets/saveTapImg/eatTitle2.png";
 import DeliveryIcon from "../assets/saveTapImg/deliveryIcon.png";
 import DeliveryTitle1 from "../assets/saveTapImg/deliveryTitle1.png";
 import DeliveryTitle2 from "../assets/saveTapImg/deliveryTitle2.png";
+import AlcoholIcon from "../assets/healthTabImg/alcoholIcon.png";
+import AlcoholTitle1 from "../assets/healthTabImg/alcoholTitle1.png";
+import AlcoholTitle2 from "../assets/healthTabImg/alcoholTitle2.png";
+import WalkIcon from "../assets/healthTabImg/walkIcon.png";
+import WalkTitle1 from "../assets/healthTabImg/walkTitle1.png";
+import WalkTitle2 from "../assets/healthTabImg/walkTitle2.png";
+import GymIcon from "../assets/healthTabImg/gymIcon.png";
+import GymTitle1 from "../assets/healthTabImg/gymTitle1.png";
+import GymTitle2 from "../assets/healthTabImg/gymTitle2.png";
 
 //util
 import {
@@ -28,6 +37,12 @@ import {
 } from "./../util/tailwindStyle";
 
 function MainPage() {
+  const [selectTab, setSelectTab] = useState(1);
+
+  const tapClick = (tabNumber: number) => {
+    setSelectTab(tabNumber);
+  };
+
   return (
     <div className={`${layout}`}>
       <div className="flex flex-col pb-11">
@@ -42,21 +57,50 @@ function MainPage() {
       {/*상단 설명 부분 */}
 
       <div className="grid grid-cols-2 bg-white rounded-full justify-between py-1 mb-12 gap-6">
-        <div className="flex left-0 cursor-pointer relative justify-center items-center">
-          <TapRound1 />
-          <img
-            src={SavingTapTitle1}
-            alt="SavingTapTitle1"
-            className=" absolute"
-          />
+        <div
+          className="flex left-0 cursor-pointer relative justify-center items-center"
+          onClick={() => tapClick(1)}
+        >
+          {selectTab === 1 ? (
+            <>
+              <TapRound1 />
+              <img
+                src={SavingTapTitle1}
+                alt="SavingTapTitle1"
+                className=" absolute"
+              />
+            </>
+          ) : (
+            <img
+              src={SavingTapTitle2}
+              alt="SavingTapTitle2"
+              className=" absolute"
+            />
+          )}
         </div>
 
-        <div className="flex right-0 cursor-pointer relative justify-center items-center">
-          <img
-            src={healthTapTitle2}
-            alt="healthTapTitle2"
-            className=" absolute"
-          />
+        <div
+          className="flex right-0 cursor-pointer relative justify-center items-center"
+          onClick={() => tapClick(2)}
+        >
+          {selectTab === 1 ? (
+            <>
+              <img
+                src={healthTapTitle2}
+                alt="healthTapTitle2"
+                className=" absolute"
+              />
+            </>
+          ) : (
+            <>
+              <TapRound1 />
+              <img
+                src={healthTapTitle1}
+                alt="healthTapTitle1"
+                className=" absolute"
+              />
+            </>
+          )}
         </div>
       </div>
       {/* 챌린지 탭 부분 */}
@@ -64,23 +108,50 @@ function MainPage() {
       <div className="grid grid-cols-2 gap-9 items-center justify-center mb-4">
         <div className={`${menuOneStyle} bg-menu-color1`}>
           <div className={`${menuIconStyle}`}>
-            <img src={CoffeeIcon} alt="Coffee Icon" />
+            {selectTab === 1 ? (
+              <img src={CoffeeIcon} alt="CoffeeIcon" />
+            ) : (
+              <img src={AlcoholIcon} alt="AlcoholIcon" />
+            )}
           </div>
 
           <div className={`${menuTitleStyle}`}>
-            <img src={CoffeeTitle1} alt="CoffeeTitle1" />
-            <img src={CoffeeTitle2} alt="CoffeeTitle2" />
+            {selectTab === 1 ? (
+              <>
+                <img src={CoffeeTitle1} alt="CoffeeTitle1" />
+                <img src={CoffeeTitle2} alt="CoffeeTitle2" />
+              </>
+            ) : (
+              <>
+                <img src={AlcoholTitle1} alt="AlcoholTitle1" />
+                <img src={AlcoholTitle2} alt="AlcoholTitle2" />
+              </>
+            )}
           </div>
         </div>
 
         <div className={`${menuOneStyle} bg-menu-color2`}>
           <div className={`${menuIconStyle}`}>
-            <img src={EatIcon} alt="CoffeeIcon" />
+            {selectTab === 1 ? (
+              <img src={EatIcon} alt="EatIcon" />
+            ) : (
+              <img src={WalkIcon} alt="WalkIcon" />
+            )}
           </div>
 
           <div className={`${menuTitleStyle}`}>
-            <img src={EatTitle1} alt="EatTitle1" />
-            <img src={EatTitle2} alt="EatTitle2" />
+            {selectTab === 1 ? (
+              <>
+                {" "}
+                <img src={EatTitle1} alt="EatTitle1" />
+                <img src={EatTitle2} alt="EatTitle2" />
+              </>
+            ) : (
+              <>
+                <img src={WalkTitle1} alt="WalkTitle1" />
+                <img src={WalkTitle2} alt="WalkTitle2" />
+              </>
+            )}
           </div>
         </div>
       </div>
@@ -88,12 +159,25 @@ function MainPage() {
       <div>
         <div className="relative bg-menu-color2 rounded-2xl text-center py-24 cursor-pointer">
           <div className="absolute flex right-0 top-2 outline-none border-none">
-            <img src={DeliveryIcon} alt="DeliveryIcon" />
+            {selectTab === 1 ? (
+              <img src={DeliveryIcon} alt="DeliveryIcon" />
+            ) : (
+              <img src={GymIcon} alt="GymIcon" />
+            )}
           </div>
 
           <div className="absolute flex flex-col items-center top-16 pl-4 gap-2">
-            <img src={DeliveryTitle1} alt="DeliveryTitle1" />
-            <img src={DeliveryTitle2} alt="DeliveryTitle2" />
+            {selectTab === 1 ? (
+              <>
+                <img src={DeliveryTitle1} alt="DeliveryTitle1" />
+                <img src={DeliveryTitle2} alt="DeliveryTitle2" />
+              </>
+            ) : (
+              <>
+                <img src={GymTitle1} alt="GymTitle1" />
+                <img src={GymTitle2} alt="GymTitle2" />
+              </>
+            )}
           </div>
         </div>
       </div>
