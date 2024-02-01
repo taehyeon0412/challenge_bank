a712569744531bb5439822ed029b346ff7174541
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2024-02-01 19:26:24 +0900

    setting(4) - 로컬스토리지, 유효성검사
    -localStorage에 넣을떄는 문자열로 변환해서 넣음
    
    -input값이 없을때 submit 안되게 막음
    -경고 문구창 input에 나오게함

--- src/Components/Common/ChallengeForm.tsx
+++ src/Components/Common/ChallengeForm.tsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
+import { useNavigate, useParams } from "react-router-dom";
 
 //component
 import Button from "Components/Common/Button";
@@ -12,7 +13,10 @@ import ToolTip from "../../assets/challengeImg/setting/common/tooltip.png";
 function ChallengeForm() {
   const [inputFocus, setInputFocus] = useState(false);
   const { control, handleSubmit, watch } = useForm();
+
   const formRef = useRef<HTMLFormElement | null>(null);
+  const navigate = useNavigate();
+  const { challengeName } = useParams();
 
   const formatNumber = (value: any) => {
     if (!value) return "";
@@ -20,13 +24,19 @@ function ChallengeForm() {
   };
   /*value를 문자열로 변환 후 숫자 형식을 지정 천 단위 구분 기호로 쉼표를 삽입 */
 
-  const inputValue = watch("priceValue");
+  /* const inputValue = watch("priceValue"); */
 
   /*   console.log(inputValue); */
 
   const onSubmit = (data: any) => {
     console.log(data);
+    localStorage.setItem("userPrice", JSON.stringify(data));
+    navigate(`/challengeinit/${challengeName}/challengestart`, {
+      replace: true,
+    });
   };
+  //localStorage에 넣을떄는 문자열로 변환해서 넣어야됨
+  //반대로 꺼낼때는 숫자로 변환해서 나오게 하기
 
   const submitForm = () => {
     if (formRef.current) {
@@ -38,6 +48,8 @@ function ChallengeForm() {
       }
     }
   };
+  // submitForm 함수는 호출 시 양식 내부의 제출 버튼 클릭을 트리거함.
+  // form안에 있는 button과 form 밖에 있는 버튼을 연결하기 위해 사용
 
   return (
     <>
@@ -55,32 +67,46 @@ function ChallengeForm() {
             <Controller
               name="priceValue"
               control={control}
+              rules={{
+                validate: (value) =>
+                  value !== "" || "가격(숫자)를 반드시 입력해 주세요!",
+              }}
               defaultValue=""
-              render={({ field: { onChange, value } }) => (
-                <input
-                  className="w-full h-14 pl-4 pr-16 py-2 bg-transparent focus:outline-none text-mintColor text-xl font-extrabold"
-                  onFocus={() => setInputFocus(true)}
-                  onBlur={() => setInputFocus(false)}
-                  value={formatNumber(value)}
-                  onChange={(e) => {
-                    let value = e.target.value; //현재값 들고오기
-                    let formattedValue = value.replace(/[^0-9]/g, ""); //숫자가 아닌 문자열제거
-                    if (
-                      formattedValue.startsWith("0") &&
-                      formattedValue.length > 1
-                    ) {
-                      formattedValue = formattedValue.substring(1);
-                    }
-                    //input의 숫자 시작값이 0이 표시되는것을 방지
-                    onChange(formattedValue);
-                  }}
-                  onKeyDown={(e) => {
-                    if (e.key === `Enter`) {
-                      e.preventDefault();
-                    }
-                  }}
-                  //Enter키 submit 방지
-                />
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <>
+                  <input
+                    className="w-full h-14 pl-4 pr-16 py-2 bg-transparent focus:outline-none text-mintColor text-xl font-extrabold"
+                    onFocus={() => setInputFocus(true)}
+                    onBlur={() => setInputFocus(false)}
+                    value={formatNumber(value)}
+                    onChange={(e) => {
+                      let value = e.target.value; //현재값 들고오기
+                      let formattedValue = value.replace(/[^0-9]/g, ""); //숫자가 아닌 문자열제거
+                      if (
+                        formattedValue.startsWith("0") &&
+                        formattedValue.length > 1
+                      ) {
+                        formattedValue = formattedValue.substring(1);
+                      }
+                      //input의 숫자 시작값이 0이 표시되는것을 방지
+                      onChange(formattedValue);
+                    }}
+                    onKeyDown={(e) => {
+                      if (e.key === `Enter`) {
+                        e.preventDefault();
+                      }
+                    }}
+                    //Enter키 submit 방지
+                  />
+                  {error && (
+                    <p className="absolute left-5 text-xs text-red-300 font-black pointer-events-none">
+                      {error.message}
+                    </p>
+                  )}
+                </>
               )}
             />
 
